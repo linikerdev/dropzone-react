146c14e6507c24cff3597135da7ebd1eee61c28c
Liniker <contato@liniker.com.br>
2019-11-25 00:17:21 -0300

    obtendo url assinada para upload, e incluindo no state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { useState } from 'react'
 import GlobalStyle from './styles/global'
 // import { Container, Content } from './style'
 import Upload from './components/Upload'
+import api from 'axios'
+import _ from 'lodash'
 
 export default () => {
   const [itens, setItems] = useState([])
 
   const fileAccepts = props => {
     const arr = Array.from(props)
-    setItems(arr)
-    console.log()
+    setItems(itens.concat(arr))
+  }
+
+  const submitTransfer = () => {
+    itens.map(async item => {
+      const getUrl = await api.post(
+        'http://localhost:3333/api/v1/manager/geturl',
+        {
+          image: item.name
+        }
+      )
+      item.url = getUrl.data.data
+      item.progress = 0
+      const index = _.findIndex(itens, { name: item.name })
+      setItems([...itens, (itens[index] = item)])
+    })
   }
 
   return (
@@ -27,6 +43,7 @@ export default () => {
       ) : (
         itens.map(item => item.name)
       )}
+      <button onClick={submitTransfer}>Enviar</button>
     </div>
   )
 }
